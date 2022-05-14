bc46e6b521fa1400a057c66c7b9685cb8740bf31
kadde92 <kadir.wafin@gmail.com>
2022-05-14 15:16:55 +0300

    Operate function basics done

--- app.js
+++ app.js
@@ -3,22 +3,43 @@
 // 1. add
 
 function add(a, b) {
-    return a + b
+    if (operate(a, b)) {
+        return a + b
+    };
 };
 
 // 2. Subtract
 function subtract(a, b) {
-    return a - b;
+    if (operate(a, b)) {
+        return a - b;
+    };
 };
 
 // 3. multiply
 
 function multiply(a, b) {
-    return a * b;
+    if (operate(a, b)) {
+        return a * b;
+    };
 };
 
 // 4. divide
 
-function divide(a,b) {
-    return a/b;
+function divide(a, b) {
+    if (operate(a, b)) {
+        return a / b;
+    };
 };
+
+
+// Operate function
+// 1. check that a and b are numbers
+// 2. check that the operator is correct
+
+function operate(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return true;
+    } else {
+        return false;
+    }
+}
