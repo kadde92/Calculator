7d1db4cecad0bd408b435bc7907a59d044022df9
kadde92 <kadir.wafin@gmail.com>
2022-05-14 14:45:55 +0300

    Add calc operation fucntions to app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+// Calc operations
+
+// 1. add
+
+function add(a, b) {
+    return a + b
+};
+
+// 2. Subtract
+function subtract(a, b) {
+    return a - b;
+};
+
+// 3. multiply
+
+function multiply(a, b) {
+    return a * b;
+};
+
+// 4. divide
+
+function divide(a,b) {
+    return a/b;
+};
