ed325533f363371fb66e563b493c824a89f3f6ed
kadde92 <kadir.wafin@gmail.com>
2022-05-17 21:30:02 +0300

    Testing the calculations, add is the only function working currently

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
+let numOne = null
+let opera = null
+let numTwo = null
+
+
 // Calc operations
 
 // 1. add
 
 function add(a, b) {
-    if (operate(a, b)) {
-        return a + b
-    };
+    return a + b
 };
 
 // 2. Subtract
 function subtract(a, b) {
-    if (operate(a, b)) {
-        return a - b;
-    };
+    return a - b;
 };
 
 // 3. multiply
 
 function multiply(a, b) {
-    if (operate(a, b)) {
-        return a * b;
-    };
+    return a * b;
 };
 
 // 4. divide
 
 function divide(a, b) {
-    if (operate(a, b)) {
-        return a / b;
-    };
+    return a / b;
+
 };
 
 
@@ -36,32 +34,36 @@ function divide(a, b) {
 // 1. check that a and b are numbers
 // 2. check that the operator is correct
 
-function operate(a, b) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return true;
-    } else {
-        return false;
-    }
-}
+// function operate(a, b) {
+//     if (typeof a === 'number' && typeof b === 'number') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-function operateTemp(a,b,c) {
-    if(b === '+') {
-        add(a,c)
-    } else if(b === '-') {
-        subtract(a,c)
-    } else if(b === '/') {
-        divide(a,c)
-    } else if(b === '*') {
-        multiply(a,c)
+function calculate(a, b, c) {
+    if (b === '+') {
+        return add(a, c)
+    } else if (b === '-') {
+        subtract(a, c)
+    } else if (b === '/') {
+        divide(a, c)
+    } else if (b === '*') {
+        multiply(a, c)
     }
 }
 
 // add the screen for the calculator
 
 const screen = document.querySelector('.screen')
-const screenContent = document.createElement('p')
-screenContent.classList.add('screenContent')
+const screenContent = document.createElement('div')
+screenContent.classList.add('screencontent')
 screen.appendChild(screenContent)
+
+const result = document.createElement('div')
+result.classList.add('result')
+screen.prepend(result)
 
 
 
@@ -70,16 +72,72 @@ screen.appendChild(screenContent)
 // number buttons, select and add eventlistener
 
 const numBtn = document.querySelectorAll('.number-buttons')
-numBtn.forEach(number => number.addEventListener('click',insertScreen));
+numBtn.forEach(number => number.addEventListener('click', insertScreen));
+
+
 
 function insertScreen(e) {
-    screenContent.textContent += e.target.textContent
-    console.log(e)
-
+    screenContent.textContent += e.target.textContent;
 }
 
 
 // operator buttons
 
+//remove eventlistener if textContent.includes(operator buttons)
+
 const operatorBtn = document.querySelectorAll('.operator-buttons')
-operatorBtn.forEach(operator => operator.addEventListener('click',insertScreen));
+operatorBtn.forEach(operator => operator.addEventListener('click', insertOperator));
+
+
+function insertOperator(e) {
+    console.log(e.key)
+    // numOone is for saving the value of the first number
+    numOne = parseInt(screenContent.textContent)
+    result.textContent = screenContent.textContent + e.target.textContent
+
+    screenContent.textContent = null;
+
+    // opera is a variable for saving the operator
+    opera = e.target.textContent
+    operatorBtn.forEach(operator => operator.removeEventListener('click', insertOperator));
+
+}
+
+// remove eventlistener after one click
+
+
+// equal button
+
+const equalBtn = document.querySelector('.equals')
+equalBtn.addEventListener('click', addResult)
+
+function addResult() {
+    numTwo = parseInt(screenContent.textContent)
+    console.log(numOne, opera, numTwo)
+    result.textContent += screenContent.textContent
+    screenContent.textContent = calculate(numOne, opera, numTwo)
+
+}
+
+
+
+
+
+
+
+// clear button
+
+const clearBtn = document.querySelector('#clear')
+clearBtn.addEventListener('click', clear)
+
+function clear() {
+    screenContent.textContent = null;
+    result.textContent = null;
+    operatorBtn.forEach(operator => operator.addEventListener('click', insertOperator));
+
+}
+
+
+
+
+// 1. 
